cf2e3619cf4c320a2e392dce527c3d1d762077a4
vyacheslavodbg <vyacheslav.massage@gmail.com>
2026-09-14 00:13:56 +0300

    Refresh cache for corrected massage app

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='massage-varna-v3';
-const ASSETS=['./','./index.html','./massage-varna.jpg','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE='massage-varna-v4';
+const ASSETS=['./','./index.html','./Hero.png','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
